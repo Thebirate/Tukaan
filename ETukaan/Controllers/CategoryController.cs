0fd257a9f1f433c0d6f74badcc584691e7918f0e
Thebirate <60749357+Thebirate@users.noreply.github.com>
2023-12-21 05:06:21 -0800

    Add project files.

---
+++ ETukaan/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ETukaan.Data;
+using ETukaan.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETukaan.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db= db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objectCategory = _db.Categories.ToList();
+            return View(objectCategory);
+        }
+    }
+}
